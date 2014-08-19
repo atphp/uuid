e0555045a7a456db89c549a3f09df2d71bc8a4fb
Andy Truong <thehongtt@gmail.com>
2014-08-19 16:12:02 +0700

    Initial commit.

---
+++ src/Pecl.php
@@ -0,0 +1,21 @@
+<?php
+
+/**
+ * @file
+ * Contains \AndyTruong\Uuid\Uuid\Pecl.
+ */
+
+namespace AndyTruong\Uuid\Uuid;
+
+/**
+ * UUID implementation using the PECL extension.
+ */
+class Pecl implements UuidInterface {
+
+  /**
+   * {@inheritdoc}
+   */
+  public function generate() {
+    return uuid_create(UUID_TYPE_DEFAULT);
+  }
+}
