46087df2979206960d2c9657227bdc105fee452c
Andy Truong <andytruong@users.noreply.github.com>
2014-08-20 10:00:10 +0700

    Update UuidGeneratorAwareTrait.php
    Add comments

--- src/UuidGeneratorAwareTrait.php
+++ src/UuidGeneratorAwareTrait.php
@@ -8,12 +8,23 @@ trait UuidGeneratorAwareTrait
     /** @var UuidInterface */
     private $uuid_generator;
 
+    /**
+     * Set uuid generator.
+     *
+     * @param \AndyTruong\Uuid\UuidInterface $uuid_generator
+     * @return self
+     */
     public function setUuidGenerator(UuidInterface $uuid_generator)
     {
         $this->uuid_generator = $uuid_generator;
         return $this;
     }
 
+    /**
+     * Get uuid generator.
+     * 
+     * @return \AndyTruong\Uuid\UuidInterface
+     */
     public function getUuidGenerator()
     {
         return $this->uuid_generator;
